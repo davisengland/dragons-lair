a5530c83e07ec16daf9f88c28a929bcb2dd0b68d
davisengland <davisrodneyengland@gmail.com>
2021-03-24 17:48:49 -0600

    Finished step 5

---
+++ server/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const massive = require('massive')
+const session = require('express-session')
+const authCtrl = require('../controllers/authController')
+const treasureCtrl = require('../controllers/treasureController')
+const PORT = 4000
+const { CONNECTION_STRING, SESSION_SECRET } = process.env
+
+app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false
+}))
+
+app.post('/auth/register', authCtrl.register)
+app.post('/auth/login', authCtrl.login)
+app.get('/auth/logout', authCtrl.logout)
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+    .then(dbInstance => {
+        app.set('db', dbInstance)
+        app.listen(PORT, () => console.log(`Server running on ${PORT}`))
+    })
+    .catch(err => console.log(err))
+
+
